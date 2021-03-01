e3b31378fb0a82159302834f688342f12c27e7ef
fabiolamariapina <fabiolamariapina@gmail.com>
2021-03-01 16:45:02 -0500

    created header compontnet

---
+++ daily-news/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Daily News</h1>
+      </div>
+    );
+  }
+}
