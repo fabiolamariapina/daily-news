5f9ef8c6a5844efbb660fada2c89ade38f950966
fabiolamariapina <fabiolamariapina@gmail.com>
2021-03-01 17:20:57 -0500

    uninstalled semantic ui

--- daily-news/src/App.js
+++ daily-news/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
+import Heading from "./components/Heading";
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <main></main>
+        <Heading />
+        <main>
+       
+        </main>
       </div>
     );
   }
