860a26f40ad98aeb8702bd4c5ae58d506d8af396
fabiolamariapina <fabiolamariapina@gmail.com>
2021-03-01 18:36:09 -0500

    centered search bar

--- daily-news/src/App.js
+++ daily-news/src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 import Heading from "./components/Heading";
-
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import styled from "styled-components";
 
 export default class App extends Component {
   render() {
+    const SearchBar = styled(Form)`
+      display: flex;
+      justify-content: center;
+    `;
     return (
       <div>
         <Heading />
         <main>
-       
+          <div>
+            <SearchBar inline>
+              <Form.Control
+                className="mb-2 mr-sm-2"
+                id="inlineFormInputName2"
+                placeholder="Search For News By Source"
+              />
+              <Button type="submit" className="mb-2">
+                Search
+              </Button>
+            </SearchBar>
+          </div>
         </main>
       </div>
     );
