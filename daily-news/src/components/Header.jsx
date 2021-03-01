e2108878d17a8fcfb5efa28514d4fb71b5a4479d
fabiolamariapina <fabiolamariapina@gmail.com>
2021-03-01 16:55:45 -0500

    installed and imported styled components and used it to center and change the font of app name/header

--- daily-news/src/components/Header.jsx
+++ daily-news/src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
 export default class Header extends Component {
   render() {
+    const AppName = styled.h1`
+      font-family: "Newsreader", serif;
+      font-weight: 800px;
+      text-align: center;
+    `;
     return (
-      <div>
-        <h1>Daily News</h1>
-      </div>
+      <header>
+        <AppName>Daily News</AppName>
+      </header>
     );
   }
 }
