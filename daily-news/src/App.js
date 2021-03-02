3b0fcb34497e16903669f5cd276e6e100dcabcc6
fabiolamariapina <fabiolamariapina@gmail.com>
2021-03-01 19:05:31 -0500

    binded handleChange and handleSubmit in constructor

--- daily-news/src/App.js
+++ daily-news/src/App.js
@@ -14,6 +14,8 @@ export default class App extends Component {
       source: "",
       searchURL: "",
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   render() {
     const SearchBar = styled(Form)`
